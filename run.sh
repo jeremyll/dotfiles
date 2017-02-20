b0c18101fe7124581b5bc5b3ae3f83fc4866f352
Jeremy Llewellyn <jeremy.llewellyn@gmail.com>
2017-02-20 01:23:30 -0800

    Add run script

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Method from github.com/robbintt - Thanks!
+
+set -e # Do not continue on error
+
+cd ~
+
+DATE=`date +"%Y%m%d"`
+OLD_DOTFILES=~/.old_dotfiles/$DATE
+
+# Ensure folders in old dotfiles folder
+mkdir -p $OLD_DOTFILES
+mkdir -p $OLD_DOTFILES/git
+mkdir -p $OLD_DOTFILES/i3
+
+# Ensure destination folders
+mkdir -p ~/.config/i3
+
+# Copy old configs to old dotifiles folder, just in case
+mv /etc/gitconfig $OLD_DOTFILES/git/gitconfig | true
+mv ~/.config/i3/config $OLD_DOTFILES/i3/config | true
+
+# Link configs inside dotfiles folder into respective folders
+ln -s ~/dotfiles/git/gitconfig /etc/gitconfig
+ln -s ~/dotfiles/i3/config ~/.config/i3/config
