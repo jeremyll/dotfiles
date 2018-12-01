4802f3cfd144da755e03e95a13e8b2945f4cbd4c
Jeremy Llewellyn <jeremy.llewellyn@gmail.com>
2018-11-30 20:30:46 -0800

    Add redshift

--- run.sh
+++ run.sh
@@ -14,6 +14,7 @@ mkdir -p $OLD_DOTFILES
 mkdir -p $OLD_DOTFILES/git
 mkdir -p $OLD_DOTFILES/i3
 mkdir -p $OLD_DOTFILES/i3status
+mkdir -p $OLD_DOTFILES/redshift
 
 # Ensure destination folders
 mkdir -p ~/.config/i3
@@ -23,9 +24,11 @@ mv /etc/gitconfig $OLD_DOTFILES/git/gitconfig || true
 mv ~/.config/i3/config $OLD_DOTFILES/i3/config || true
 mv ~/.config/i3/status $OLD_DOTFILES/i3status/config || true
 mv ~/.vimrc $OLD_DOTFILES/.vimrc || true
+mv ~/.config/redshift.conf $OLD_DOTFILES/redshift || true
 
 # Link configs inside dotfiles folder into respective folders
 ln -f -s ~/.dotfiles/git/gitconfig /etc/gitconfig
 ln -f -s ~/.dotfiles/i3/config ~/.config/i3/config
 ln -f -s ~/.dotfiles/i3/status ~/.config/i3/status
 ln -f -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -f -s ~/redshift.conf ~/.config/redshift.conf
